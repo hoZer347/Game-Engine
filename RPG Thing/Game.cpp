5a96d2a8514b1f6acc81f4a16e19ed4f27630765
hoZer347 <liam@browndomain.com>
2021-10-24 13:25:06 -0400

    bz

--- RPG Thing/Game.cpp
+++ RPG Thing/Game.cpp
@@ -25,18 +25,33 @@ void Game::init() {
         int w=0, h=0;
         glfwGetWindowSize(room->r->window, &w, &h);
 
-        vec3 ray = vec3(-1.0f + (2.0f * i->mx)/w, 1.0f - (2.0f * i->my)/h, 1);
-        vec4 clp = vec4(ray.x, ray.y, -1, 1);
-        vec4 rye = inverse(c->proj) * clp;
-        vec4 eye = vec4(rye.x, rye.y, -1, 0);
-        vec3 wye = vec3(inverse(c->view) * rye);
-        wye = normalize(wye);
+        // https://antongerdelan.net/opengl/raycasting.html
+
+       vec4 v = vec4(0);
+
+        vec3 ray_cds = vec3(-1.0f + (2.0f * i->mx)/w, 1.0f - (2.0f * i->my)/h, 1);
+        vec4 ray_clp = vec4(ray_cds.x, ray_cds.y, -1, 1);
+
+        vec4 ray_eye = inverse(c->proj) * ray_clp;
+        ray_eye = vec4(ray_eye.x, ray_eye.y, -1, 0);
+        vec3 ray_wld = vec3(inverse(c->view) * ray_eye);
+        ray_wld = normalize(ray_wld);
         
+        v = vec4(ray_wld, 1);
+
+        std::cout <<
+            v.x << "," <<
+            v.y << "," <<
+            v.z << "," <<
+            std::endl;
+
+        Mesh* m = &MESH[1];
+
+        *m = vec3(v);
 
         i->update();
         room->r->update();
 
         millisec_since_epoch = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - millisec_since_epoch;
-        std::cout << millisec_since_epoch << std::endl;
     }
 }

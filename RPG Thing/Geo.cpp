d8fa12e46932398035c168d6597c0c9e688eda8f
hoZer347 <34010435+hoZer347@users.noreply.github.com>
2022-03-11 18:12:44 -0500

    dcjkc

---
+++ RPG Thing/Geo.cpp
@@ -0,0 +1,5 @@
+#include "Geo.h"
+
+namespace geo {
+
+};
---
+++ RPG Thing/Geo.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Mesh.h"
+
+namespace geo {
+	class Geo :
+		public mesh::Mesh {
+
+	};
+	
+	class Cube :
+		public Geo {
+	public:
+		
+
+	private:
+
+	};
+
+
+};
